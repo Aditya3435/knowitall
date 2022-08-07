549a3b59a615b3779076a9ae63fb5980929abee2
Aditya Maurya <adityamaurya3435@gmail.com>
2022-08-07 23:19:27 +0530

    added map and reviews

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,26 @@
 document.querySelector('.home-btn').addEventListener('click', () => {
     document.getElementById('home-display').style.setProperty('display', 'initial');
     document.getElementById('fact-display').style.setProperty('display', 'none');
+    document.getElementById('map-display').style.setProperty('display', 'none');
+    document.getElementById('adm-display').style.setProperty('display', 'none');
 })
 
 document.querySelector('.info-btn').addEventListener('click', () => {
     document.getElementById('fact-display').style.setProperty('display', 'initial');
     document.getElementById('home-display').style.setProperty('display', 'none');
+    document.getElementById('map-display').style.setProperty('display', 'none');
+    document.getElementById('adm-display').style.setProperty('display', 'none');
+})
+
+document.querySelector('.map-btn').addEventListener('click', () => {
+    document.getElementById('map-display').style.setProperty('display', 'initial');
+    document.getElementById('fact-display').style.setProperty('display', 'none');
+    document.getElementById('adm-display').style.setProperty('display', 'none');
+    document.getElementById('home-display').style.setProperty('display', 'none');
+})
+document.querySelector('.adm-btn').addEventListener('click', () => {
+    document.getElementById('adm-display').style.setProperty('display', 'initial');
+    document.getElementById('fact-display').style.setProperty('display', 'none');
+    document.getElementById('home-display').style.setProperty('display', 'none');
+    document.getElementById('map-display').style.setProperty('display', 'none');
 })
