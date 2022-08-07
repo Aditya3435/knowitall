04eb5a3be98154b312aa573e291a48d262113706
Aditya Maurya <adityamaurya3435@gmail.com>
2022-08-07 13:08:25 +0530

    made navbar and homepage

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+document.querySelector('.home-btn').addEventListener('click', () => {
+    document.getElementById('home-display').style.setProperty('display', 'initial');
+    document.getElementById('fact-display').style.setProperty('display', 'none');
+})
+
+document.querySelector('.info-btn').addEventListener('click', () => {
+    document.getElementById('fact-display').style.setProperty('display', 'initial');
+    document.getElementById('home-display').style.setProperty('display', 'none');
+})
