faf5610b861f90f1f34df4c902f1a6a160eced23
Aditya Maurya <adityamaurya3435@gmail.com>
2022-08-08 10:14:10 +0530

    added image slider

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,11 @@
 'use strict';
 
+
 document.querySelector('.home-btn').addEventListener('click', () => {
     document.getElementById('home-display').style.setProperty('display', 'initial');
     document.getElementById('fact-display').style.setProperty('display', 'none');
     document.getElementById('map-display').style.setProperty('display', 'none');
+    document.getElementById('image-display').style.setProperty('display', 'none');
     document.getElementById('adm-display').style.setProperty('display', 'none');
 })
 
@@ -12,6 +14,7 @@ document.querySelector('.info-btn').addEventListener('click', () => {
     document.getElementById('home-display').style.setProperty('display', 'none');
     document.getElementById('map-display').style.setProperty('display', 'none');
     document.getElementById('adm-display').style.setProperty('display', 'none');
+    document.getElementById('image-display').style.setProperty('display', 'none');
 })
 
 document.querySelector('.map-btn').addEventListener('click', () => {
@@ -19,10 +22,21 @@ document.querySelector('.map-btn').addEventListener('click', () => {
     document.getElementById('fact-display').style.setProperty('display', 'none');
     document.getElementById('adm-display').style.setProperty('display', 'none');
     document.getElementById('home-display').style.setProperty('display', 'none');
+    document.getElementById('image-display').style.setProperty('display', 'none');
 })
 document.querySelector('.adm-btn').addEventListener('click', () => {
     document.getElementById('adm-display').style.setProperty('display', 'initial');
     document.getElementById('fact-display').style.setProperty('display', 'none');
     document.getElementById('home-display').style.setProperty('display', 'none');
     document.getElementById('map-display').style.setProperty('display', 'none');
+    document.getElementById('image-display').style.setProperty('display', 'none');
+})
+
+document.querySelector('.img-btn').addEventListener('click', () => {
+    document.getElementById('image-display').style.setProperty('display', 'initial');
+    document.getElementById('adm-display').style.setProperty('display', 'none');
+    document.getElementById('fact-display').style.setProperty('display', 'none');
+    document.getElementById('home-display').style.setProperty('display', 'none');
+    document.getElementById('map-display').style.setProperty('display', 'none');
+    document.querySelector('nav').style = "z-index: 2";
 })
